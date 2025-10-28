3376dea11114fd240b368f88fae0f02b001a7639
Jowohny <vujohny109238@gmail.com>
2025-10-27 23:20:51 -0700

    basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const App = () => {
+	return (
+    <nav className="border-b flex place-items-center">
+      <div className="mx-auto px-6 py-4">
+        <div className="flex gap-4">
+					<button className={`px-4 py-2 rounded font-bold border ${ location.pathname === '/' ? 'bg-blue-400 text-white' : 'bg-white'}`}>
+						Inventory
+					</button>
+					<button className={`px-4 py-2 rounded font-bold border ${ location.pathname === '/categories' ? 'bg-blue-400 text-white' : 'bg-white'}`}>
+						Categories
+					</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </nav>
+  );
+};
 
-export default App
+export default App;
