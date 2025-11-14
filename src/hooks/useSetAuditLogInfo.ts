998adfcde3f68a7092952aed2430daaf8d499b9f
Jowohny <vujohny109238@gmail.com>
2025-11-13 23:19:47 -0800

    audit log to firebase setup

---
+++ src/hooks/useSetAuditLogInfo.ts
@@ -0,0 +1,22 @@
+import { addDoc, collection, deleteDoc, getDocs, query, where } from "firebase/firestore";
+import { db } from "../config/firebase-config";
+
+export const useSetAuditLogInfo = () => {
+	const auditLogRef = collection(db, 'audit logs');
+
+	const addDBAudit = async (message: string, user: string, time: Date) => {
+		await addDoc(auditLogRef, {
+			message,
+			user,
+			time
+		});
+	}
+
+	const clearDBAudits = async () => {
+		const snapshot = await getDocs(auditLogRef);
+
+		snapshot.forEach((doc) => { deleteDoc(doc.ref) })
+	}
+
+	return { addDBAudit, clearDBAudits }
+}
