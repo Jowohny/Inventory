2799cb87d3aa8448a4f734091b7541cf1de42978
Jowohny <vujohny109238@gmail.com>
2025-11-15 20:51:14 -0800

    item db adjustment added

--- src/hooks/useSetItemInfo.ts
+++ src/hooks/useSetItemInfo.ts
@@ -9,15 +9,12 @@ export const useSetItemInfo = () => {
 		const docSnapshot = await getDoc(containerDoc);
 		if (docSnapshot.exists()) {
 			const itemList = [...(docSnapshot.data().items || []), item];
-
 			await updateDoc(containerDoc, { items: itemList });
-
 			return {
 				id: containerID,
 				name: docSnapshot.data().name,
 				items: itemList
-			}
-			
+			};
 		} else {
 			return null;
 		}
@@ -31,26 +28,58 @@ export const useSetItemInfo = () => {
 			const itemList = (docSnapshot.data().items || []).map((item: Item) => item.id !== itemID);
 
 			if (itemList.length === 0) {
-				await updateDoc(containerDoc, {items: itemList})
+				await updateDoc(containerDoc, {items: itemList});
 				return {
 					id: containerID,
 					name: docSnapshot.data().name,
 					items: itemList
-				}
+				};
 			} else {
-				await updateDoc(containerDoc, { items: [] })
+				await updateDoc(containerDoc, { items: [] });
 				return {
 					id: containerID,
 					name: docSnapshot.data().name,
 					items: []
-				}
+				};
 			}			
-
 
 		} else {
 			return null;
 		}
 	}
 
-	return { addItemToContainer, deleteItemFromContainer };
+	const adjustItemQuantityFromContainer = async (containerID: string, itemID: string, quantity: number) => {
+    const containerDoc = doc(db, "containers", containerID);
+    const docSnapshot = await getDoc(containerDoc);
+
+    if (docSnapshot.exists()) {
+			const data = docSnapshot.data();
+			const items: Item[] = data.items || [];
+
+			const index = items.findIndex(item => item.id === itemID);
+
+			const previousQuantity = items[index].quantity;
+
+			const updatedItems = [...items];
+			updatedItems[index] = {
+					...updatedItems[index],
+					quantity: quantity,
+			};
+
+			await updateDoc(containerDoc, {
+					items: updatedItems,
+			});
+
+			return {
+					difference: quantity - previousQuantity,
+					updatedItem: updatedItems[index],
+					items: updatedItems,
+			};
+		} else {
+			return null;
+		}
+};
+
+
+	return { addItemToContainer, deleteItemFromContainer, adjustItemQuantityFromContainer };
 }
