f07a62534504ad95df3d3cb6a776206b6fdbe255
Jowohny <vujohny109238@gmail.com>
2025-11-17 23:09:49 -0800

    bug fix and user grab

--- src/hooks/useGetAuditLogInfo.ts
+++ src/hooks/useGetAuditLogInfo.ts
@@ -1,32 +1,51 @@
-import { collection, endAt, onSnapshot, orderBy, query, startAt } from "firebase/firestore"
+import { collection, onSnapshot, query, where } from "firebase/firestore"
 import { db } from "../config/firebase-config";
 import type { Audit } from "../interface";
 
 export const useGetAuditLogInfo = () => {
 	const auditLogRef = collection(db, 'audit logs');
 
-	const getAuditLogs = async (nameFilter: string, onUpdate: (auditLogs: Audit[]) => void ) => {
+	const getDBAuditLogs = (nameFilter: string, onUpdate: (auditLogs: Audit[]) => void ) => {
 		let q;
 		
 		if (nameFilter) {
 			q = query(
 				auditLogRef,
-				orderBy('name'),
-				startAt(nameFilter),
-				endAt(nameFilter + '\uf8ff')
-			) 
+				where('user', '==', nameFilter)
+			);
 		} else {
-			q = query(auditLogRef)
+			q = query(auditLogRef);
 		}
 
 		const unsubscribe = onSnapshot(q, (snapshot) => {
 			const list = snapshot.docs.map((doc) => ({
 				message: doc.data().message,
-				time: doc.data().time,
+				time: doc.data().time.toDate(),
 				user: doc.data().user
-			}))
+			}));
 
-			onUpdate(list)
-		})
+			onUpdate(list);
+		});
+
+		return unsubscribe;
 	}
+
+	const getUniqueUsers = (onUpdate: (users: string[]) => void) => {
+		const q = query(auditLogRef);
+	
+		const unsubscribe = onSnapshot(q, (snapshot) => {
+			const uniqueUsers = new Set<string>();
+	
+			snapshot.docs.forEach((doc) => {
+				uniqueUsers.add(doc.data().user);
+			});
+	
+			onUpdate([...uniqueUsers]);
+		});
+	
+		return unsubscribe;
+	};
+	
+
+	return { getDBAuditLogs, getUniqueUsers };
 }
