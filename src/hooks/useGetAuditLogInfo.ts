7d5d50e736cb65d0af426387b44e8a919398641d
Jowohny <vujohny109238@gmail.com>
2026-03-16 22:29:09 -0700

    limit auditlog container reads

--- src/hooks/useGetAuditLogInfo.ts
+++ src/hooks/useGetAuditLogInfo.ts
@@ -1,48 +1,34 @@
-import { collection, getDocs, orderBy, query, where } from "firebase/firestore"
+import { collection, getDocs, orderBy, query, where, limit, startAfter, QueryDocumentSnapshot } from "firebase/firestore";
 import { db } from "../config/firebase-config";
 import type { Audit } from "../interface";
 
 export const useGetAuditLogInfo = () => {
 	const auditLogRef = collection(db, 'audit logs');
 
-	const getDBAuditLogs = async (nameFilter: string): Promise<Audit[]> => {
-		let q;
-		
-		if (nameFilter) {
-			q = query(
-				auditLogRef,
-				where('user', '==', nameFilter),
-				orderBy('time', 'desc')
-			);
-		} else {
-			q = query(
-				auditLogRef,
-				orderBy('time', 'desc')
-			);
-		}
+	const getDBAuditLogs = async (nameFilter: string, lastDoc: QueryDocumentSnapshot | null = null) => {
+    let q = query(auditLogRef, orderBy('time', 'desc'), limit(10));
+    
+    if (nameFilter) {
+      q = query(q, where('user', '==', nameFilter));
+    }
 
-		const snapshot = await getDocs(q);
-		const list = snapshot.docs.map((doc) => ({
-			message: doc.data().message,
-			time: doc.data().time.toDate(),
-			user: doc.data().user
-		}));
+    if (lastDoc) {
+      q = query(q, startAfter(lastDoc));
+    }
 
-		return list;
-	}
+    const snapshot = await getDocs(q);
+    
+	  const list: Audit[] = snapshot.docs.map((doc) => ({
+      message: doc.data().message,
+      time: doc.data().time.toDate(),
+      user: doc.data().user
+    }));
 
-	const getUniqueUsers = async (): Promise<string[]> => {
-		const q = query(auditLogRef);
-		const snapshot = await getDocs(q);
-	
-		const uniqueUsers = new Set<string>();
-		snapshot.docs.forEach((doc) => {
-			uniqueUsers.add(doc.data().user);
-		});
-	
-		return [...uniqueUsers];
-	};
-	
+    return {
+      list,
+      lastVisible: snapshot.docs[snapshot.docs.length - 1]
+    };
+  }
 
-	return { getDBAuditLogs, getUniqueUsers };
+	return { getDBAuditLogs };
 }
