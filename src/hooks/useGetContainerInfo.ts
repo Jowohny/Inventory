2089996255b21fae0bf2fc1d2231b8abaf4c0d68
Jowohny <vujohny109238@gmail.com>
2026-03-25 18:27:17 -0700

    back to container snapshot hook
    going back to this since too much regular container loading, better anyway

--- src/hooks/useGetContainerInfo.ts
+++ src/hooks/useGetContainerInfo.ts
@@ -1,38 +1,27 @@
-import { collection, orderBy, getDoc, query, doc, getDocs } from "firebase/firestore";
+import { collection, onSnapshot, query } from "firebase/firestore";
 import { db } from "../config/firebase-config";
+import type { Container } from "../interface";
 
 export const useGetContainerInfo = () => {
-	const containerRef = collection(db, "containers");
+  const containerRef = collection(db, "containers");
 
-	const getDBContainers = async () => {
-    const q = query(containerRef, orderBy('name', 'asc'));
-    const snapshot = await getDocs(q);
+  const getDBContainers = (         
+    onUpdate: (containers: Container[]) => void
+  ) => {
+    const q = query(containerRef);
 
-    const list = snapshot.docs.map(doc => {
-      const data = doc.data();
-      return {
-        id: doc.id, 
-        name: data.name || "Unnamed Container",
-        items: data.items || []
-      };
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      let list: Container[] = snapshot.docs.map(doc => ({
+        id: doc.id,
+        name: doc.data().name,
+        items: doc.data().items || []
+      }));
+
+      onUpdate(list);
     });
 
-    return list;
+    return unsubscribe;
   };
-	const getDBContainerFromId = async (id: string) => {
-		const containerDoc = doc(db, 'containers', id)
-		const docSnapshot = await getDoc(containerDoc);
 
-		if (docSnapshot.exists()) {
-			return {
-				id: docSnapshot.id,
-				name: docSnapshot.data().name,
-				items: docSnapshot.data().items
-			};		
-		} else {
-			return null;
-		}
-	}
-
-	return { getDBContainers, getDBContainerFromId };
+  return { getDBContainers };
 };
