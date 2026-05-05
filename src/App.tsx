e2694fd31b1679fcd76ed1a81aeb9ebec673f9d5
Jowohny <vujohny109238@gmail.com>
2026-05-04 19:41:34 -0700

    limit inventory listeners to data pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,38 @@ import Categories from "./pages/Categories";
 import AuditLogs from "./pages/AuditLogs";
 import Login from "./pages/Login"
 import { InventoryProvider } from "./contexts/InventoryContext";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import { useGetCurrentUser } from "./hooks/useGetCurrentUser";
 
+const InventoryDataRoutes = () => {
+	const { isAuth } = useGetCurrentUser();
+
+	if (!isAuth) {
+		return <Navigate to="/" replace />;
+	}
+
+	return (
+		<InventoryProvider>
+			<Outlet />
+		</InventoryProvider>
+	);
+}
 
 const App = () => {
 	return (
-		<InventoryProvider>
+		<>
 			<div className="flex justify-center round-full">
 				<img className="w-3/4 md:w-1/4 rounded-full" src="/logo.jpg"/>
 			</div>
 			<Routes>
 				<Route path='/' element={<Login />}/>
-				<Route path='/inventory' element={<Inventory />}/>
-				<Route path='/categories' element={<Categories />}/>
+				<Route element={<InventoryDataRoutes />}>
+					<Route path='/inventory' element={<Inventory />}/>
+					<Route path='/categories' element={<Categories />}/>
+				</Route>
 				<Route path='/auditlogs' element={<AuditLogs />}/>
 			</Routes>
-		</InventoryProvider>
+		</>
 	);
 }
 
